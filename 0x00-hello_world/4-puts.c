42588db0725c5c0dcf77edea9f87c4e725b115fd
weldsh2535 <weldetsadik2535@gmail.com>
2022-12-08 12:08:09 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
- int main(void){
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Sucess)
+ */
+ int main(void)
+{
 	 puts("\"Programming is like building a multilingual puzzle");
 	 return (0);
  }
