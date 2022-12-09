7e9e182a27456c4c65b9c98518e7de0f2916072a
weldsh2535 <weldetsadik2535@gmail.com>
2022-12-09 12:18:14 +0300

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,11 +10,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("%s %d is %d and is ", last, n, n % 10);
-	if(n % 10 > 5)
+	if (n % 10 > 5)
 	{
 		printf("greater than 5\n");
 	}
-	else if(n % 10 == 0)
+	else if (n % 10 == 0)
 	{
 		printf("0\n");
 	}
