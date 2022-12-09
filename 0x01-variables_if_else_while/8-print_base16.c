2eae1f7f2454b9138cfee2fb1b5882ae3b1027c4
Weldsh Aweke <74667144+weldsh2535@users.noreply.github.com>
2022-12-09 12:50:06 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
-
+/**
+ * main - prints all numbers of base 16.
+(*
+ * Return: 0 on success
+ */
 int main(void)
 {
 	int i = '0';
 	char c = 'a';
 
-	while(i <= '9')
+	while (i <= '9')
 	{
 		putchar(i);
 		i++;
 	}
 
-	while(c <= 'f')
+	while (c <= 'f')
 	{
 		putchar(c);
 		c++;
