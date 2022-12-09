b4f65983f29f0978fe937afc0099cb72e08ed3bb
Weldsh Aweke <74667144+weldsh2535@users.noreply.github.com>
2022-12-09 12:36:17 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - print all the letters except q and e
+ * main - Prints the alphabet except the letters q and e.
+(*
  * Return: 0 on success
  */
 int main(void)
 {
 	char c = 'a';
-	
+
 	while (c <= 'z')
 	{
 		if (c != 'q' && c != 'e')
