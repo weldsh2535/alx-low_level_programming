b14d85b6af3116ebbccd04e7338c04a6befefcd3
weldsh2535 <weldetsadik2535@gmail.com>
2022-12-13 17:50:01 +0300

    104 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define LARGEST 10000000000
 /**
- * main - main block 
+ * main - main block
  * Description: Find and print the first 98 fib numbers starting with 1 and 2.
  * Numbers should be coma and space separated.
  * Return: 0
