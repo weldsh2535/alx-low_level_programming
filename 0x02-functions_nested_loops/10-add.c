dd0d8e6a67eed48b4f23db883fa8e8edde7c1fd8
weldsh2535 <weldetsadik2535@gmail.com>
2022-12-13 17:22:14 +0300

    add two number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Sum does enters per función.
+ * @i: Primry varable.
+ * @l: Second variable.
+ * Return: return the numberof sum
+ */
+int add(int i, int l)
+{
+	int n;
+	 n = i + l;
+	return (n);
+}
