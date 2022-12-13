8b05990f4523af19b9bd096add581bcfc54873da
weldsh2535 <weldetsadik2535@gmail.com>
2022-12-13 16:58:20 +0300

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - checks number
+ * @n: .
+ * Return: 1 if c is lowercase
+ */
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
