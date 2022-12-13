e5263b63ab5f7c7d2bf38620ed8a2444f5b26ff3
weldsh2535 <weldetsadik2535@gmail.com>
2022-12-13 17:09:31 +0300

    6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - checks number
+ * @r: character.
+ * Return: Positive r cuando as values absolute,
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
