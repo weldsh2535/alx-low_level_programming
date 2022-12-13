9dcdd2822d53bdb59bc817b90caede082c476d35
weldsh2535 <weldetsadik2535@gmail.com>
2022-12-13 13:23:37 +0300

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,19 @@
-#include <stdio.h>
+
+
+#include "main.h"
 /* to display or prints _putchar
  *
  */
-int main(void)
-{
-	printf("_putchar\n");
+int main(void){
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
