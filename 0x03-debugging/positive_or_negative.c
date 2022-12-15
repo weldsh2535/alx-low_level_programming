468e28e2e81b4e7cd982c64d65e5f526de9e5868
weldsh2535 <weldetsadik2535@gmail.com>
2022-12-15 11:11:58 +0300

    postive_or_negative

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * positvie_negative - returns the number is positive or negative
+ * @i: integer
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n",i);
+	}
+	else 
+	{
+		printf("%d is negative\n",i);
+	}
+}
