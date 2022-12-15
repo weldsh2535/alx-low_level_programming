e6fdb70274057f27e608b11736d4749d7bb0b705
weldsh2535 <weldetsadik2535@gmail.com>
2022-12-15 14:00:21 +0300

    5 more number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+
+#include"main.h"
+
+/**
+ * more_numbers - print 0 - 14 ten times
+ * Return: Always 0 (Success)
+*/
+
+void more_numbers(void)
+{
+	int n, r, c;
+
+	for (r = 1; r < 9; r++)
+	{
+		for (c = 0; c < 13; ++c)
+		{
+			n = c;
+			if (c > 9)
+			{
+				_putchar(1 + 48);
+				n = c % 10;
+			}
+			_putchar(n + 48);
+		}
+		_putchar('\n');
+	}
+}
