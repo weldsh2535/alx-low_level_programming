5b05036df372edf50884dd5d62d0c51b2bc47914
weldsh2535 <weldetsadik2535@gmail.com>
2022-12-15 13:56:07 +0300

    print most number

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,10 +11,9 @@ void print_most_numbers(void)
 
 	for (n = 0; n <= 9; n++)
 	{
-		if (n != 2 || n != 4)
-		{
-			_putchar(n + 48);
-		}
+		if (n == 2 || n == 4)
+			continue;
+		_putchar(n + 48);
 	}
 	_putchar('\n');
 }
