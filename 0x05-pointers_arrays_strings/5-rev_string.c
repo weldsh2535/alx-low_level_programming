adcec07c26562a00c0a1fd64faa88176daa84a95
weldsh2535 <weldetsadik2535@gmail.com>
2022-12-19 11:29:55 +0300

    5. A good engineer thinks in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+/**
+ * rev_string - reverses a string
+ * @s: string parameter input
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int length, index;
+	char ch;
+
+	for (length = 0; s[length] != '\0'; ++length)
+		;
+
+	for (index = 0; index < length / 2; ++index)
+	{
+		ch = s[index];
+		s[index] = s[length - 1 - index]; 
+		s[length - 1 - index] = ch;
+	}
+
+}
